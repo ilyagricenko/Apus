0dcd1b3765fc87e5d712365b3c206c2b49f7f021
ilya4 <ilya41473@gmail.com>
2023-01-26 15:38:28 +0200

    first commit

---
+++ src/main/java/com/booksservice/apus/ApusApplication.java
@@ -0,0 +1,13 @@
+package com.booksservice.apus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booksservice/apus/ApusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksservice.apus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
